0803ea7c591c02584f373b22e5c21b14700f866b
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2020-08-04 18:39:34 +0800

    [CppExpr] create CppExpr, cppExprRender and cppAndAll

---
+++ src/AsmJsonCpp/CppExpr.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StrictData #-}
+
+module AsmJsonCpp.CppExpr
+  ( CppExpr (..),
+    cppExprRender,
+    cppAndAll
+    )
+where
+
+import qualified Data.Text.Lazy as L
+import RIO
+import RIO.List.Partial
+
+data CppExpr
+  = EVarLiteral L.Text
+  | EInfixFn L.Text CppExpr CppExpr
+  | EMethodCall CppExpr L.Text ~[CppExpr]
+  | EBoolLiteral Bool
+  | EStringLiteral L.Text
+  | ENumberLiteral Int
+  | -- | Render anything for you.
+    EWorkaround L.Text
+  deriving (Show)
+
+cppExprRender :: CppExpr -> L.Text
+cppExprRender (EVarLiteral var) = var
+cppExprRender (EInfixFn fn l r) = cppExprRender l <> " " <> fn <> " " <> cppExprRender r
+cppExprRender (EMethodCall expr method args) =
+  cppExprRender expr <> "." <> method <> "(" <> argsRender args <> ")"
+cppExprRender (EBoolLiteral True) = "true"
+cppExprRender (EBoolLiteral False) = "false"
+cppExprRender (ENumberLiteral n) = L.pack $ show n
+cppExprRender (EStringLiteral s) = "\"" <> s <> "\""
+cppExprRender (EWorkaround s) = s
+
+-- | Convert [exprA, exprB, exprC] into "exprA && exprB && exprC"
+cppAndAll :: Foldable t => t CppExpr -> CppExpr
+cppAndAll cs
+  | null cs = EBoolLiteral True
+  | otherwise = foldr1 (EInfixFn "&&") cs
+
+argsRender :: [CppExpr] -> L.Text
+argsRender = L.intercalate ", " . fmap cppExprRender
