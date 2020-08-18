08e8d67c41f8858fe302b8a9f5b16f8253922684
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2020-08-18 15:07:02 +0800

    [Parser] create & export parseAsmJson

---
+++ src/AsmJsonCpp/Parser.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module AsmJsonCpp.Parser
+  ( Parser,
+    asmJson,
+    parseAsmJson,
+  )
+where
+
+import AsmJsonCpp.Asm
+import Control.Applicative.Combinators hiding (many, some)
+import qualified Data.Text.Lazy as L
+import RIO hiding (many, some, try)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as Lex
+
+type Parser a = Parsec Void L.Text a
+
+parseAsmJson :: L.Text -> Either (ParseErrorBundle L.Text Void) AsmJson
+parseAsmJson = parse asmJson "INPUT"
+
+asmJson :: Parser AsmJson
+asmJson = lexeme $ choice [asInt, asString, asObj, asArray]
+  where
+    asInt = AsInt <$ lastKeyword "AsInt"
+
+    asString = AsString <$ lastKeyword "AsString"
+
+    asObj =
+      AsObj <$ keyword "AsObj"
+        <*> choice [atField, atFields]
+    atField =
+      AtField <$ keyword "AtField"
+        <*> fieldName
+        <*> asmJson
+    atFields =
+      AtFields <$ keyword "AtFields"
+        <*> array field
+
+    asArray =
+      AsArray <$ keyword "AsArray"
+        <*> choice [eachElement, atNth]
+    eachElement =
+      EachElement <$ keyword "EachElement"
+        <*> asmJson
+    atNth =
+      AtNth <$ keyword "AtNth"
+        <*> nth
+        <*> asmJson
+
+fieldName :: Parser L.Text
+fieldName = lexeme (fromString <$> (some alphaNumChar))
+
+nth :: Parser Int
+nth = lexeme Lex.decimal
+
+array :: Parser a -> Parser [a]
+array p = between (symbol "[") (symbol "]") $ do
+  ps <- many (try $ p <* symbol ",")
+  lastP <- p
+  return $ ps <> [lastP]
+
+field :: Parser (L.Text, AsmJson)
+field = between (symbol "(") (symbol ")") $ do
+  name <- lexeme . fmap fromString $ some alphaNumChar
+  _ <- symbol ","
+  asm <- lexeme $ asmJson
+
+  return (name, asm)
+
+lexeme :: Parser a -> Parser a
+lexeme = Lex.lexeme space
+
+keyword :: L.Text -> Parser L.Text
+keyword t = lexeme . try $ string t <* lookAhead space1
+
+-- | Like 'token', but don't require trailing space.
+lastKeyword :: L.Text -> Parser ()
+lastKeyword t = void $ string t
+
+symbol :: L.Text -> Parser L.Text
+symbol t = Lex.symbol space t
