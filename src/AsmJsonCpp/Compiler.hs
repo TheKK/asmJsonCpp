9e259643de41608a1113ef8ee3b57738e23d2f2e
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2020-08-22 16:37:49 +0800

    Revert "[Compiler] create TypeComputeState"
    This reverts commit fbb83517b58b18cfa37f520c799148bd37d776d6.

--- src/AsmJsonCpp/Compiler.hs
+++ src/AsmJsonCpp/Compiler.hs
@@ -39,13 +39,6 @@ compileToCppFn fnName asm =
 
 compileToResultType :: AsmJson -> CppCV -> CppType
 compileToResultType asm cv = evalState (compileToResultType' asm cv) infiniteUpperNames
-
-data TypeComputeState = TypeComputeState
-  { typeNames :: [L.Text],
-    discoveredTypes :: [CppType]
-  }
-  deriving (Show)
-
   where
     infiniteUpperNames = flip replicateM ['T' .. 'Z'] =<< [1 ..]
 
