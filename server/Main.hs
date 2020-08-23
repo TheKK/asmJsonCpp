d7626dcdeeacf343a24a85d06f4bba264e717706
Ying-Ruei Liang(KK) <thumbd03803@gmail.com>
2020-08-24 00:50:11 +0800

    [server.Main] impl api to compile AsmJson to cpp source code
    api/v1/compile with query string "query"

--- server/Main.hs
+++ server/Main.hs
@@ -1,7 +1,37 @@
-module Main
-  ( main,
-  )
-where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Main (main) where
+
+import AsmJsonCpp.Compiler
+import AsmJsonCpp.Parser
+import qualified Data.Text.Lazy as L
+import qualified Network.Wai.Handler.Warp as Warp
+import Network.Wai.Middleware.RequestLogger
+import RIO
+import qualified RIO.ByteString.Lazy as BL
+import Servant
+
+type Api = "api" :> "v1" :> "compile" :> QueryParam' '[Required, Strict] "query" L.Text :> Get '[PlainText] L.Text
+
+server :: Server Api
+server = compile_v1
+
+compile_v1 :: L.Text -> Servant.Handler L.Text
+compile_v1 input =
+  case parseAsmJson $ input of
+    Left err -> throwError $ err400 {errBody = BL.fromStrict . encodeUtf8 . L.toStrict $ err}
+    Right asm -> return $ compileToFullCppSourceCode asm
+
+api :: Proxy Api
+api = Proxy
+
+app :: Application
+app = serve api server
 
 main :: IO ()
-main = pure ()
+main =
+  Warp.runEnv 1234 $
+    app
+      & logStdoutDev
